7d1283cc13575c69c703cb688ffdda4e0e97f61e
viktor <viktor@acklenavenue.com>
2015-11-07 00:04:48 -0600

    fixed buttons and text positioning

--- client/app/scripts/controllers/HomeController.js
+++ client/app/scripts/controllers/HomeController.js
@@ -1,14 +1,19 @@
 angular.module('Zblog.Controllers')
-  .controller('HomeController', ['$scope', '$moment', '$state', 'postService', 'toaster', function ($scope, $moment, $state, postService, toaster) {
+  .controller('HomeController', ['$scope', '$moment', 'postService', 'toaster', function ($scope, $moment, postService, toaster) {
     	$scope.posts = {};
     	postService.GetAll().then(function(response){
-    		$scope.posts = response.data;
+    		$scope.posts = _.map(response.data, function(element){
+                return {
+                    _id: element._id,
+                    title: element.title,
+                    subtitle: element.subtitle,
+                    body: element.body,
+                    author: element.author,
+                    date: $moment(element.date).fromNow()
+                }
+            });
 
     	}).catch(function(err){
     		toaster.pop('error', 'Oh noes!', 'We had trouble getting your posts. Heres the error: ' + err);
     	});
-
-    	$scope.CreatePost = function(){
-    		$state.go('newPost');
-    	}
   }]);
